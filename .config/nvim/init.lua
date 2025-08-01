4b94a92c0232ac57ca3df45689f3832ea0a65c81
Julian Grimm <grimmjulian225@gmail.com>
2025-08-01 14:37:18 +0200

    feat(neovim): add treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,14 +9,43 @@ vim.opt.winborder = "rounded"
 
 vim.g.mapleader = " "
 
+vim.keymap.set('n', '<leader>so', ':update<CR>:source<CR>')
+vim.keymap.set('n', '<leader>ee', ':Exp<CR>')
+-- Colorscheme
+--
+
+vim.pack.add({
+	{ src = "https://github.com/EdenEast/nightfox.nvim" },
+})
+vim.cmd("colorscheme carbonfox")
+-- Treesitter
+
+vim.pack.add({
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+})
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "r", "dockerfile"},
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		---@diagnostic disable-next-line: unused-local
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024   -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+		additional_vim_regex_highlighting = false,
+	},
+}
+
+-- LSP
+--
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
-
-vim.keymap.set('n', '<leader>so', ':update<CR>:source<CR>')
-vim.keymap.set('n', '<leader>ee', ':Exp<CR>')
-
--- LSP
 vim.lsp.config('lua_ls', {
 	on_init = function(client)
 		if client.workspace_folders then
@@ -55,9 +84,9 @@ vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
----@diagnostic disable-next-line: need-check-nil
+		---@diagnostic disable-next-line: need-check-nil
 		if client:supports_method('textDocument/completion') then
----@diagnostic disable-next-line: need-check-nil
+			---@diagnostic disable-next-line: need-check-nil
 			vim.lsp.completion.enable(true, client.id, ev.buf)
 		end
 	end
