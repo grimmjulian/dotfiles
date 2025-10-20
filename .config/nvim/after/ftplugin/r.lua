5b259836b565a92f87e756a6ad1b3c4c38931726
Julian Grimm <grimmjulian225@gmail.com>
2025-10-20 17:16:35 +0200

    fix local variable

--- .config/nvim/after/ftplugin/r.lua
+++ .config/nvim/after/ftplugin/r.lua
@@ -4,7 +4,7 @@ vim.opt.expandtab = false
 
 
 local function is_r_ready()
-	bufnr = vim.api.nvim_get_current_buf()
+	local bufnr = vim.api.nvim_get_current_buf()
 	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
 	-- Iterate backwards to find the last non-empty line
