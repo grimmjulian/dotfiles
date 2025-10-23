f84001f924507cc1ed2224c0bf7ef8e7eb4bc312
Julian Grimm <grimmjulian225@gmail.com>
2025-10-23 10:26:28 +0200

    Add better commit browser

--- .config/nvim/lua/git.lua
+++ .config/nvim/lua/git.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/tpope/vim-fugitive" }
+	{ src = "https://github.com/tpope/vim-fugitive" },
+	{ src = "https://github.com/junegunn/gv.vim" },
 })
 
 require("gitsigns").setup({
@@ -13,6 +14,5 @@ require("gitsigns").setup({
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-vim.keymap.set('n', '<leader>gl', function()
-  vim.cmd('Git log --oneline --all --graph --decorate')
-end, { desc = 'Git log (oneline graph)', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gl', vim.cmd.GV, { desc = 'Git log (oneline graph)', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gfl', function()vim.cmd("GV!")end, { desc = 'Git log (oneline graph)', noremap = true, silent = true })
