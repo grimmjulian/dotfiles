5593f568da0816ef274d2171f6fdcb834eccba51
Julian Grimm <grimmjulian225@gmail.com>
2025-11-28 12:33:51 +0100

    Add some basic R Pickleterm stuff for testing

--- .config/nvim/after/ftplugin/r.lua
+++ .config/nvim/after/ftplugin/r.lua
@@ -9,3 +9,13 @@ vim.keymap.set("n",
 	"<leader>dc",
 	function() pt.send_cmd("R_CMD", "echo 'hello'") end
 )
+
+vim.keymap.set("n", "<leader>sl",
+	function() pt.send_line("R") end)
+
+vim.keymap.set("v", "<leader>ss",
+	function() pt.send_visual("R") end)
+vim.keymap.set("v", "<leader>sl",
+	function() pt.send_visual("R") end)
+vim.keymap.set("v", "<leader>s",
+	function() pt.send_visual("R") end)
